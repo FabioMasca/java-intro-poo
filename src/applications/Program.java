db6f6fe8ea291f72d0f41bd91cbf1a03eebd6835
Fabio Mascarenhas <fabio.mascarenhas2209@gmail.com>
2024-02-01 14:22:43 -0300

    feat: first commit

---
+++ src/applications/Program.java
@@ -0,0 +1,31 @@
+package applications;
+
+import entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter rectangle widht and height: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.println("Area = "+String.format("%.2f",rectangle.area()));
+        System.out.println("Perimeter = "+String.format("%.2f",rectangle.perimeter()));
+        System.out.println("Diagonal = "+String.format("%.2f",rectangle.diagonal()));
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
